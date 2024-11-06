3de9796b8acd3cd0029ebbdb09f621153fd5e31b
William01br <williamstcorreia123@gmail.com>
2024-11-06 15:34:57 -0300

    route api/register + create DB

--- routes/logins.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const router = express.Router();
-
-// get page login
-router.get("/login", (req, res) => {
-  res.render("login", { erro: req.query.erro });
-});
-
-export default router;
--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
-import login from "./routes/logins.js";
 import { pool } from "./models/db.js";
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/api/register", (req, res) => {
+  const { name, email, password } = req.body;
+
+  pool.query(
+    "INSERT INTO users (name, email, password) VALUES ($1, $2, $3)",
+    [name, email, password],
+    (err, result) => {
+      if (err) {
+        console.error("Error inserting user:", err);
+        return res.status(500).send("Error inserting user");
+      }
+      console.log("User inserted successfully:", result.rows);
+      return res.status(201).send("User registered successfully");
+    }
+  );
+});
 
 const testDbConnection = async () => {
   try {
