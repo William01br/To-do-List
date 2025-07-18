3b92521333b3a05c50583c19f70ba8209174b259
William01br <williamstcorreia123@gmail.com>
2025-07-18 14:42:00 -0300

    refactor: refactor errors handlers

--- src/middleware/errorMiddleware.js
+++ src/middleware/errorMiddleware.js
@@ -1,15 +1,25 @@
+import HttpError from "../errors/HttpError.js";
+
 export const errorHandler = (err, req, res, next) => {
-  console.error(
-    JSON.stringify({ message: err.message, code: err.statusCode }, null, 2),
-    err.stack
-  );
-  const errStatus = err.statusCode || 500;
-  const errMsg = err.message || "Something went wrong";
-  res.status(errStatus).json({
-    sucess: false,
-    status: errStatus,
-    message: errMsg,
-  });
+  const { statusCode, message, context } = err;
+  if (err instanceof HttpError) {
+    console.warn(
+      JSON.stringify({ code: err.statusCode, message: err.message }, null, 2),
+      err.stack
+    );
+    res.status(statusCode).json({
+      errors: [
+        {
+          message: message,
+          context: context,
+        },
+      ],
+    });
+    return;
+  }
+  console.error(err);
+  res.status(500).json({ errors: [{ message: "Something went wrong" }] });
+  return;
 };
 
 export const notFoundHandler = (req, res, next) => {
